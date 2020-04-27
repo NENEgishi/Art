e48522c5c38f121b553a857ce8886e16be535ecf
NENEgishi <gn575094@gmail.com>
2020-04-27 11:54:08 +0000

    bookers4

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'startes/top'
+  get 'negishes/new'
+  get 'negishes/index'
+  get 'negishes/edit'
+  get 'negishes/show'
+  get 'negishes/create'
+  get 'negishes/destroy'
+  get 'negishes/update'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
