6502111ef88f8c17ac95381443281222ab173682
NENEgishi <gn575094@gmail.com>
2020-04-28 14:40:39 +0000

    修正

--- app/controllers/negishes_controller.rb
+++ app/controllers/negishes_controller.rb
@@ -1,22 +1,33 @@
 class NegishesController < ApplicationController
   def new
+    @negishi=Negishi.new
   end
 
   def index
+    @negishes=Negishi.all
   end
 
   def edit
   end
 
   def show
+    @negishi=Negishi.find(params[:id])
   end
 
   def create
+    negishi=Negishi.new(negishi_params)
+    negishi.save
+    redirect_to negishes_path
   end
 
   def destroy
   end
 
   def update
+  end
+
+  private
+  def negishi_params
+    params.require(:negishi).permit(:title, :body)
   end
 end
