9f6695f1cae2cd142ea5ea9ca30049c5e8d31674
NENEgishi <gn575094@gmail.com>
2020-05-02 16:53:24 +0000

    解決

--- app/controllers/negishes_controller.rb
+++ app/controllers/negishes_controller.rb
@@ -37,6 +37,6 @@ class NegishesController < ApplicationController
 
   private
   def negishi_params
-    params.require(:negishi).permit(:title, :body, :image)
+    params.require(:negishi).permit(:title, :body)
   end
 end
