31448e7ab44d08cf4fb0b2a4ed2d491e5757818a
NENEgishi <gn575094@gmail.com>
2020-05-27 06:58:40 +0000

    devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_27_114645) do
+ActiveRecord::Schema.define(version: 2020_05_27_065638) do
 
   create_table "negishis", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,19 @@ ActiveRecord::Schema.define(version: 2020_04_27_114645) do
   create_table "starts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
